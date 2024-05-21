870decb8e1d64ce48f1f9d114270898168b566b2
keattt0330 <133674934+keattt0330@users.noreply.github.com>
2024-05-22 03:40:53 +0800

    login

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import SearchJob from './searchJob.jsx';
-import ListUser from './test.jsx'; 
+import ListUser from './test.jsx';
+import UserLogin from './loginPage.jsx';
 
 function App() {
     return (
         <>
-            <Header />
+            {/* <Header />
             <SearchJob /> 
-            <Footer />
-            {/* <ListUser/> */}
+            <Footer /> */}
+            <UserLogin/>
         </>
     );
 }
--- my-react-app/src/loginPage.jsx
+++ my-react-app/src/loginPage.jsx
@@ -1,1 +1,20 @@
-
+export default function UserLogin() {
+    return (
+        <section className="userLoginContainer">
+          <div>
+            <h1>Welcome Back!</h1>
+            <form className="userLoginForm">
+              <label htmlFor="email">Email Address</label>
+              <input type="email" id="email" name="email" required />
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+              <div className="rememberForgot">
+                <label><input type="checkbox" />Remember me</label>
+                <a href="#">Forgot password?</a>
+              </div>
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </section>
+    );
+}
