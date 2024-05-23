85fc632cb786f4f157b91d79dfbf7af3e8e3f658
Chew Jun Jie <168407722+JiakSai@users.noreply.github.com>
2024-05-23 23:45:06 +0800

    Register api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('/job-categories', [JobCategoryController::class, 'index']);
+Route::post('/UserRegister', [UserController::class, 'createUser']);
+Route::post('/Userlogin', [UserController::class, 'loginUser']);
