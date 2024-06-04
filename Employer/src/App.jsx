6fa050ca6dbde7380db7d71148542416dd1e5aa6
Ammar Afzan <125238102+zarkmuckerber@users.noreply.github.com>
2024-06-04 19:34:34 +0800

    Add files via upload

---
+++ Employer/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+import Header from './Header.jsx';
+import Editjob from './Editjob.jsx';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Editjob />
+    </>
+
+  )
+}
+
+export default App
