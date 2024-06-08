aaea4c5c9282ab55396b21c5aff89c7bedba4777
Ammar Afzan <125238102+zarkmuckerber@users.noreply.github.com>
2024-06-09 04:37:12 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css';
 import Header from './Header.jsx';
 import Editjob from './Editjob.jsx';
+import Addjob from './Addjob.jsx';
+import Editprofile from './editProfile.jsx';
 
 function App() {
   return (
     <>
-    <Header />
-    <Editjob />
+    <Header /> 
+    {/* <Editjob /> edit this later after addjob is done */}
+    {/* <Addjob /> */}
+    <Editprofile />
     </>
 
   )
